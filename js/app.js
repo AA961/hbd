e98d9740eb578633c7ce4fece21c91760ade435c
AA961 <ammar.ahmed961@gmail.com>
2022-06-02 09:08:29 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,122 @@
+let body = document.querySelector("body")
+
+
+body.addEventListener("click", () => {
+
+    let audio = new Audio()
+
+    audio.src = "./static/baloo.wav"
+
+    setTimeout(() => {
+        audio.play()
+
+    }, 900);
+
+
+    const x = event.offsetX
+    const y = event.offsetY
+
+    let spans = document.createElement("span")
+    spans.classList.add("baloon")
+    spans.style.left = x + "px"
+    spans.style.top = y + "px"
+
+    let size = Math.random() * 300
+
+    spans.style.width = size + "px"
+    spans.style.height = size + "px"
+
+    body.appendChild(spans)
+    setTimeout(() => {
+        spans.remove()
+    }, 1000);
+}
+
+)
+function heart2() {
+    let heart = document.createElement("span")
+    heart.style.left = Math.random() * 1000 + "px"
+    heart.style.top = Math.random() * 1500 + "px"
+
+    let size = Math.random() * 200
+
+    heart.classList.add("heart2")
+    heart.style.width = size + "px"
+    heart.style.height = size + "px"
+
+    body.appendChild(heart)
+    setTimeout(() => {
+        heart.remove()
+    }, 3000);
+}
+
+function hearts() {
+    let heart = document.createElement("span")
+    heart.style.left = Math.random() * 1000 + "px"
+    heart.style.top = Math.random() * 3000 + "px"
+
+    let size = Math.random() * 200
+
+    heart.classList.add("hearts")
+    heart.style.width = size + "px"
+    heart.style.height = size + "px"
+
+    body.appendChild(heart)
+    setTimeout(() => {
+        heart.remove()
+    }, 3000);
+}
+
+function heartBaloon() {
+    let heart = document.createElement("span")
+    heart.style.left = Math.random() * 1000 + "px"
+    heart.style.top = Math.random() * 3000 + "px"
+
+    let size = Math.random() * 100
+
+    heart.classList.add("heart-baloon")
+    heart.style.width = size + "px"
+    heart.style.height = size + "px"
+
+    body.appendChild(heart)
+    setTimeout(() => {
+        heart.remove()
+    }, 3000);
+}
+
+function baloon() {
+    let heart = document.createElement("span")
+    heart.style.left = Math.random() * 1000 + "px"
+    heart.style.top = Math.random() * 3000 + "px"
+
+    let size = Math.random() * 400
+
+    heart.classList.add("baloon2")
+    heart.style.width = size + "px"
+    heart.style.height = size + "px"
+
+    body.appendChild(heart)
+    setTimeout(() => {
+        heart.remove()
+    }, 4000);
+
+}
+
+
+
+
+setTimeout(() => {
+    setInterval(() => {
+        baloon()
+    }, 300);
+}, 5000);
+
+
+setTimeout(() => {
+    setInterval(() => {
+        hearts()
+        heart2()
+        heartBaloon()
+    }, 800);
+
+}, 7000);
